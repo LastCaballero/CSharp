ab9949299c4f0ed977cf953c07da4398e20c4195
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-10-28 10:59:28 +0200

    Add files via upload

---
+++ file_receiver.cs
@@ -0,0 +1,50 @@
+using System.Net ;
+using System.Net.Sockets ;
+using System.IO ;
+
+
+
+class NetcatContact {
+	TcpListener me ;
+	TcpClient netcat ;
+	IPAddress my_ip ;
+	public NetworkStream nc_stream { get ; } 
+
+	public NetcatContact( int port ) {
+		my_ip = IPAddress.Parse("127.0.0.1");
+		me = new TcpListener( my_ip, port ) ;
+		me.Start() ;
+		netcat = me.AcceptTcpClient() ;
+		nc_stream = netcat.GetStream() ;
+	}
+}
+
+class StreamExchange {
+	StreamReader outside ;
+	StreamWriter inside ;
+	NetworkStream nc_stream ;
+	public StreamExchange ( string filename , ref NetworkStream nc ) {
+		outside = new StreamReader( nc ) ;
+		inside = new StreamWriter( filename ) ;
+		nc_stream = nc ;
+	}
+	public void Exchange () {
+		while ( nc_stream.DataAvailable ) {
+			inside.WriteLine( outside.ReadLine() ) ;			
+		}
+	}
+
+}
+
+
+
+class Start {
+	static int port ;
+	static string filename ;
+	public static void Main ( string[] args ) {
+		port = int.Parse( args[0] ) ;
+		filename = args[1] ;
+		NetworkStream nc = new NetcatContact( port ).nc_stream ;
+		new StreamExchange( filename, ref nc ).Exchange() ;
+	}
+}
