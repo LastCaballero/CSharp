b0a53fb67828271bf04bb12a25f4d03c597ccadc
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-10-29 14:45:20 +0200

    Add files via upload

---
+++ netcat_dialog.cs
@@ -0,0 +1,58 @@
+using System ;
+using System.Net.Sockets ;
+using System.IO ;
+
+
+class Contacter {
+	
+	public TcpClient 		NetCat 		{ get ; }
+	public NetworkStream 	NCStream 	{ get ; }
+	public StreamReader 	FromNetcat 	{ get ; }
+	public StreamWriter 	ToNetcat 	{ get ; }
+	
+	public Contacter ( string host , int port ) {
+		
+		NetCat 		= new 	TcpClient( host, port ) ;	
+		NCStream 	= NetCat.GetStream() ;
+		FromNetcat 	= new 	StreamReader( NCStream ) ;
+		
+		ToNetcat	= new 	StreamWriter( NCStream ) ;
+		ToNetcat.AutoFlush = true ;
+			
+		TalkToNetcat() ;
+	
+	}
+
+	void TalkToNetcat () {
+		string line ;
+		while ( true )  {
+			line = Console.ReadLine() ;
+			ToNetcat.WriteLine( line ) ;
+			ReadFromNetcat() ;
+		}
+	}
+
+	void ReadFromNetcat () {
+		string line ;
+		if ( NCStream.DataAvailable ) {
+			while ( true ) {
+				line = FromNetcat.ReadLine() ;
+				Console.WriteLine( line ) ;
+				if ( ! NCStream.DataAvailable )
+					break ;
+			}
+		}
+	}
+
+}
+
+
+
+
+
+
+class Start {	
+	public static void Main ( string[] args ) {
+		new Contacter( args[0] , int.Parse(args[1]) ) ;	
+	}
+}
