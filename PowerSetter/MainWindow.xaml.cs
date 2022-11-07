7c7bfa9ef5734c058f91d1d27c646bfef20cc6e3
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-11-07 21:24:10 +0100

    Update MainWindow.xaml.cs

--- PowerSetter/MainWindow.xaml.cs
+++ PowerSetter/MainWindow.xaml.cs
@@ -5,9 +5,6 @@ using System.Diagnostics ;
 
 namespace energie
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
