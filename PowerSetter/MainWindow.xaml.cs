6c17d9985287f4a4b59f5cbc621ff3b13fd0d634
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-11-07 21:20:42 +0100

    Add files via upload

---
+++ PowerSetter/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Diagnostics ;
+
+
+
+namespace energie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        void SaveEnergie(object sender, RoutedEventArgs e)
+        {
+             Process.Start("powercfg.exe","/setactive scheme_max") ;
+        }
+
+        void BalancedMode(object sender, RoutedEventArgs e)
+        {
+            Process.Start("powercfg.exe","/setactive scheme_balanced") ;
+        }
+        
+        void PowerMode(object sender, RoutedEventArgs e)
+        {
+             Process.Start("powercfg.exe","/setactive scheme_min") ;
+
+        }
+    }
+}
