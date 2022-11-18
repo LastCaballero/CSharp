0cd6eeb472c696d84183a63beb93a3c83b197808
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-11-18 09:07:18 +0100

    Add files via upload

--- TcpScanner/tcpscanner.cs
+++ TcpScanner/tcpscanner.cs
@@ -1,6 +1,8 @@
 using System ;
 using System.Net.Sockets ;
+using System.Collections.Concurrent ;
 using System.Threading.Tasks ;
+using System.Threading ;
 
 
 class ScanBubble {
@@ -8,46 +10,62 @@ class ScanBubble {
 	string 	Host ;
 	int 	Start ;
 	int 	End ;
-	
+	public ConcurrentBag<int> open { get ; set ; }
+	public ConcurrentBag<int> closed { get ; set ; }
 	
 	public ScanBubble (string host, int start, int end ) {
 		Host = host ;
 		Start = start ;
 		End = end ;
+		open = new ConcurrentBag<int>() ;
+		closed = new ConcurrentBag<int>() ;
+		StartScanning() ;
+	}
+	async Task StartScanning() {
+		await ScanRange() ;
+		Thread.Sleep(3000) ;
+		ShowOpen() ;
+		ShowClosed() ;
+	}
+	void ShowOpen() {
+		foreach( int p in open ) {
+			Console.WriteLine( p + " open" ) ;
+		}
+	}
+	void ShowClosed() {
+		foreach( int p in closed ) {
+			Console.WriteLine( p + " closed" ) ;
+		}
 	}
 	
-	public void ScanRange() {
+	async Task ScanRange() {
 		for ( int i = Start ; i <= End ; i++ ) {
-			ScanPort ( i ) ;						
+			new Thread( () => ScanPort(i) ).Start() ;
 		}
 	}
 
-	public async Task ScanPort ( int port ) {
+	public void ScanPort ( int port ) {
 		TcpClient spider = new TcpClient() ;
 		try {
 			spider.Connect( Host , port ) ;
 		} catch { 
-			Console.WriteLine( port + "\tclosed" ) ;
+			closed.Add( port  ) ;
 			return ;
 		}
 		if ( spider.Connected ) {
-			Console.WriteLine( port + "\topen" ) ;
+			open.Add( port ) ;
 			spider.Dispose() ;
 		}
-		
-	}
+	} 
 }
 
 
 class Start {
-	static ScanBubble bubble ;
 	
 	public static void Main( string[] args ) {
 		string host = args[0] ;
 		int start 	= int.Parse( args[1] ) ;
 		int end 	= int.Parse( args[2] ) ;
-		bubble = new ScanBubble( host, start, end ) ;
-		Console.WriteLine("Ports at " + host) ;
-		bubble.ScanRange() ;
+		new ScanBubble( host, start, end ) ;
 	}
 }
