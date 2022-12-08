744da12e7364ee1dd51a3803ac76ac24af604c28
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-12-08 15:08:37 +0100

    Add files via upload

---
+++ fontviewer/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System ;
+using System.Windows.Media ;
+
+namespace fontviewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            
+            foreach (var font in Fonts.SystemFontFamilies )
+            {
+                var combo_item = new ComboBoxItem();
+                combo_item.Content = font;
+                combo_item.FontFamily = font ;
+                combo_item.FontSize = 16 ;
+                Dispatcher.Invoke(
+                    () => { Family.Items.Add(combo_item); }
+                );
+
+            }
+        }
+
+    }
+}
+
