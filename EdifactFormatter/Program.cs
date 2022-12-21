cb6af6b481c12a11b44431f702da84118dc938fc
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-12-21 22:11:49 +0100

    Add files via upload

---
+++ EdifactFormatter/Program.cs
@@ -0,0 +1,16 @@
+﻿using edifact ;
+using messages ;
+
+
+
+
+
+class Start {
+
+    public static void Main(string[] args) {
+        Console.Write(
+            new Edifact( SampleMessages.One )
+        ) ;
+    }
+}
+
