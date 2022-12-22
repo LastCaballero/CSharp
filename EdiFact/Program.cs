49282720e5ace433579fa6bd979c31b0628e385e
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-12-22 07:37:02 +0100

    Add files via upload

---
+++ EdiFact/Program.cs
@@ -0,0 +1,18 @@
+﻿using edifact;
+using System.IO;
+
+
+class Start
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 1)
+            Console.Error.WriteLine("we need at least one argument - path to a file.");
+        
+        if (File.Exists(args[0]))
+            Console.Write(new Edifact(args[0]));
+        else
+            Console.Error.WriteLine("the file on the command line don´t exists... ") ;
+    }
+}
+
