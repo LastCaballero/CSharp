360d5ec30cc9168c774735d31bbcc6b58b9766b8
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-12-26 20:24:21 +0100

    Add files via upload

--- PowerSetter/MainWindow.xaml.cs
+++ PowerSetter/MainWindow.xaml.cs
@@ -1,32 +1,56 @@
 ﻿using System.Windows;
 using System.Diagnostics ;
 
-
-
 namespace energie
 {
+   
     public partial class MainWindow : Window
     {
+        Process Power ;
         public MainWindow()
         {
             InitializeComponent();
-
+            Power = new Process() ;
+            ConfigureProcess() ;
+            ShowScheme();
+            
+        }
+        void ConfigureProcess(){
+            Power.StartInfo.FileName = "powercfg.exe" ;
+            Power.StartInfo.UseShellExecute = false ;
+            Power.StartInfo.CreateNoWindow = true ;
+            Power.StartInfo.RedirectStandardOutput = true ;
         }
 
         void SaveEnergie(object sender, RoutedEventArgs e)
         {
-             Process.Start("powercfg.exe","/setactive scheme_max") ;
+            Power.StartInfo.Arguments = "/setactive scheme_max" ;
+            Power.Start() ;
+            ShowScheme();
         }
 
         void BalancedMode(object sender, RoutedEventArgs e)
         {
-            Process.Start("powercfg.exe","/setactive scheme_balanced") ;
+            Power.StartInfo.Arguments = "/setactive scheme_balanced" ;
+            Power.Start() ;
+            ShowScheme();
         }
         
         void PowerMode(object sender, RoutedEventArgs e)
         {
-             Process.Start("powercfg.exe","/setactive scheme_min") ;
-
+            Power.StartInfo.Arguments = "/setactive scheme_min" ;
+            Power.Start() ;
+            ShowScheme();
+        }
+        string GetActiveMode() {
+            Power.StartInfo.Arguments = "/getactivescheme" ;
+            if( Power.Start() )
+                return Power.StandardOutput.ReadLine() ;
+            else
+                return "No Information available..." ;
+        }
+        void ShowScheme() {
+            Scheme.Content = GetActiveMode() ;
         }
     }
 }
