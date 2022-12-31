1315d9eb5622e95b225e94ded1dd2da2651822c2
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-12-31 14:40:12 +0100

    Add files via upload

---
+++ BrowserAffinityBooster/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+class AffinityBooster
+{
+    string firefox = "firefox";
+    string msedge = "msedge";
+    int StandardAffinity = 1 + 2 + 4 + 8 ;
+    List<Process> ?BrowserProcesses = new List<Process>() ;
+    public AffinityBooster()
+    {
+        FindBrowssers() ;
+        SetAffinity() ;
+    }
+    public AffinityBooster(int affinity) {
+        StandardAffinity = affinity ;
+        FindBrowssers() ;
+        SetAffinity() ;
+    }
+    void FindBrowssers()
+    {
+        BrowserProcesses = Process.GetProcessesByName(firefox).ToList() ;
+        BrowserProcesses.AddRange(Process.GetProcessesByName(msedge).ToList()) ;
+    }
+    void SetAffinity(){
+        BrowserProcesses?.ForEach(
+            browser => { 
+                    browser.ProcessorAffinity = StandardAffinity ;
+                } 
+        ) ;
+    }
+}
+
+
+
+class Start
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 1)
+        {
+            int affi = int.Parse(args[0]) ;
+            new AffinityBooster(affi) ;    
+        } else
+        {
+            new AffinityBooster() ;    
+        }
+        
+    }
+}
