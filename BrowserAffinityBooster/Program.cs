4ac98d390bb2d334b9faa504189ebfd6e1ecaf9d
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-12-31 17:29:04 +0100

    Add files via upload

--- BrowserAffinityBooster/Program.cs
+++ BrowserAffinityBooster/Program.cs
@@ -4,29 +4,28 @@ class AffinityBooster
 {
     string firefox = "firefox";
     string msedge = "msedge";
-    int StandardAffinity = 1 + 2 + 4 + 8 ;
-    List<Process> ?BrowserProcesses = new List<Process>() ;
-    public AffinityBooster()
+    int StandardAffinity;
+    List<Process>? BrowserProcesses = new List<Process>();
+    public AffinityBooster() : this(1 + 2 + 4 + 8) { }
+    public AffinityBooster(int affinity)
     {
-        FindBrowssers() ;
-        SetAffinity() ;
-    }
-    public AffinityBooster(int affinity) {
-        StandardAffinity = affinity ;
-        FindBrowssers() ;
-        SetAffinity() ;
+        StandardAffinity = affinity;
+        FindBrowssers();
+        SetAffinity();
     }
     void FindBrowssers()
     {
-        BrowserProcesses = Process.GetProcessesByName(firefox).ToList() ;
-        BrowserProcesses.AddRange(Process.GetProcessesByName(msedge).ToList()) ;
+        BrowserProcesses = Process.GetProcessesByName(firefox).ToList();
+        BrowserProcesses.AddRange(Process.GetProcessesByName(msedge).ToList());
     }
-    void SetAffinity(){
+    void SetAffinity()
+    {
         BrowserProcesses?.ForEach(
-            browser => { 
-                    browser.ProcessorAffinity = StandardAffinity ;
-                } 
-        ) ;
+            browser =>
+            {
+                browser.ProcessorAffinity = StandardAffinity;
+            }
+        );
     }
 }
 
@@ -38,12 +37,13 @@ class Start
     {
         if (args.Length == 1)
         {
-            int affi = int.Parse(args[0]) ;
-            new AffinityBooster(affi) ;    
-        } else
-        {
-            new AffinityBooster() ;    
+            int affi = int.Parse(args[0]);
+            new AffinityBooster(affi);
         }
-        
+        else
+        {
+            new AffinityBooster();
+        }
+
     }
 }
